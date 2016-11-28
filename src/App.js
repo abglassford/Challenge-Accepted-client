5cd89249505c14e0486ccac44b2f7ba8a1da6176
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-28 15:22:42 -0700

    add facebook login button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <FacebookLogin
+          appId="1473688849309792"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={responseFacebook} />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
