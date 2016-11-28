78edd570410cca91524aff6aca4bf8412df9e111
Alexander Glassford <a.b.glassford@gmail.com>
2016-11-28 16:37:23 -0700

    fix errors

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Navbar from './Nav.js'
 
 class Home extends Component {
   render () {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, Redirect, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
 import './index.css';
 import Home from './Home';
