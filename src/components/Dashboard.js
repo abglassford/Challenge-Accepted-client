4f9ff4dda93f308401a4f05f4972cba043ee9634
Alexander Glassford <a.b.glassford@gmail.com>
2016-12-01 15:39:01 -0700

    style splash a little

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 
-export default class Home extends Component {
-  constructor () {
-    super ()
-  }
-
+export default class Dashboard extends Component {
   render () {
     return (
       <div>
         <NavBar />
-        <h1>Hey</h1>
+        <h1>This is the dashboard</h1>
       </div>
     )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import '../css/home.css';
 
 export default class Home extends Component {
-  constructor () {
-    super ()
-  }
-
   render () {
     return (
-      <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+      <div className="jumbotron banner">
+        <div className="banner-content">
+          <h4>Accept the Challenge...</h4>
+          <h1 className="banner-title">Challenge Accepted!</h1>
+          <p className="banner-button"><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
       </div>
     )
   }
